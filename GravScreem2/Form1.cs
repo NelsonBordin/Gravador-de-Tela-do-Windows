c5cbd2570c226536320d3172eb180661c7c3e705
NelsonBordin <nelsonbordin@hotmail.com>
2023-12-23 09:44:35 -0300

    inicial do projeto

---
+++ GravScreem2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+
+namespace GravScreem2
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mat frame = new Mat();
+            Bitmap screenshot = CaptureScreen();
+            screenshot.Save("screenshot.png");
+        }
+
+        static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        static Bitmap CaptureScreen()
+        {
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+            Bitmap screenshot = new Bitmap(screenWidth, screenHeight);
+            using (Graphics g = Graphics.FromImage(screenshot))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, screenshot.Size);
+            }
+            return screenshot;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Altere o valor de qualidade conforme necessário (0-100)
+        }
+    }
+}
